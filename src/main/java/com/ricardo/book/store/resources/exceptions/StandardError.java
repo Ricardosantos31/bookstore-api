4209b2072cd7a75235a146e80c9bd6fdbaee8fe3
Ricardosantos31 <riacrdok9@gmail.com>
2022-01-14 16:13:30 -0400

    excecao personalizada para objeto nao encontrado

---
+++ src/main/java/com/ricardo/book/store/resources/exceptions/StandardError.java
@@ -0,0 +1,42 @@
+package com.ricardo.book.store.resources.exceptions;
+
+public class StandardError {
+
+    private Long timestemp;
+    private Integer status;
+    private String error;
+
+    public StandardError(){
+        super();
+    }
+
+    public StandardError(Long timestemp, Integer status, String error) {
+        this.timestemp = timestemp;
+        this.status = status;
+        this.error = error;
+    }
+
+    public Long getTimestemp() {
+        return timestemp;
+    }
+
+    public void setTimestemp(Long timestemp) {
+        this.timestemp = timestemp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
--- src/main/java/com/ricardo/book/store/service/CategoriaService.java
+++ src/main/java/com/ricardo/book/store/service/CategoriaService.java
@@ -16,6 +16,7 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
