9607a39916a09ccfe39b2982815e8c31e452ac8b
Ricardosantos31 <riacrdok9@gmail.com>
2022-01-14 15:30:37 -0400

    Algumas alterações

--- src/main/java/com/ricardo/book/store/service/CategoriaService.java
+++ src/main/java/com/ricardo/book/store/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.ricardo.book.store.service;
 
 import com.ricardo.book.store.domain.Categoria;
 import com.ricardo.book.store.repositories.CategoriaRepository;
+import com.ricardo.book.store.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,8 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repository;
 
-    public Categoria findById(Integer id)
-    {
+    public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
