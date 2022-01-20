48d5cea3aefc01fc878b4897383af5f82e92329b
Ricardosantos31 <riacrdok9@gmail.com>
2022-01-20 16:32:34 -0400

    DELETE para Categoria

--- src/main/java/com/ricardo/book/store/resources/CategoriaResource.java
+++ src/main/java/com/ricardo/book/store/resources/CategoriaResource.java
@@ -48,6 +48,12 @@ public class CategoriaResource {
         Categoria newObj = service.update(id, objDto);
         return ResponseEntity.ok().body(new CategoriaDTO(newObj));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
 
 //localhost:8080/categorias\
