270bec0ec4ad80aa5b2e6b0dcc4721c3b224ff4e
Ricardosantos31 <riacrdok9@gmail.com>
2022-01-26 22:40:11 -0400

    DELETE para Livro

--- src/main/java/com/ricardo/book/store/resources/LivroResource.java
+++ src/main/java/com/ricardo/book/store/resources/LivroResource.java
@@ -53,5 +53,11 @@ public class LivroResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping(value = ("/{id}"))
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
